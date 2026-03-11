41ec9a00d7da64a2b1f7b0cd8c3d497c3db720ce
Joshua Sachtleben <joshuasachtleben@gmail.com>
2026-03-11 13:02:31 -0400

    fix: insert proper tab indents in ModalBlocker to prevent GDScript parser failure

--- ui/ModalBlocker.gd
+++ ui/ModalBlocker.gd
@@ -1,14 +1,14 @@
 extends "res://systems/input/InputProcessor.gd"
 
 func handleStart():
-pass
+	pass
 
 func buttonEvent(event) -> bool:
-if justPressed(event, "ui_cancel"):
-var p = get_parent()
-if p and p.has_method("_on_cancel"):
-p._on_cancel()
-return true
+	if justPressed(event, "ui_cancel"):
+		var p = get_parent()
+		if p and p.has_method("_on_cancel"):
+			p._on_cancel()
+	return true
 
 func stick_move(event) -> bool:
-return true
+	return true
